2386a05f4994d369e3f602dc943a1cf3360e2837
Heloisa Cabral <heloisacabralco@gmail.com>
2024-03-01 19:36:06 -0300

    conexao com banco

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+
 from django.db import models
 
-# Create your models here.
+class UploadFile(models.Model):
+    document = models.FileField(upload_to='uploads/')
--- app/views.py
+++ app/views.py
@@ -1,22 +1,27 @@
-from urllib import request
-
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
+from .models import UploadFile
 
 
 class Home(TemplateView):
     template_name = 'home.html'
 
+
 def upload(request):
     context = {}
-    if request.method == 'POST': #se o metodo no html for um post
-        uploaded_file = request.FILES['document'] #aqui ele ta pegando o documento do front
-        # print(uploaded_file.name) # . name é uma propriedade padrao que os arquivos tem dentro do django
-        # agora va la no settings pra fazer o media_root e o media_url
-        #vamos puxar a api agora
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file) #ja esta salvando no caminho que nos colocamos no media_root, e se pesquisarmos media/nomedoarquivo vamos conseguir ver esse arquivo na tela
-        #o caminho media nao tem nada a ver o nome da pasta, ele está como media porque definimos no settings
-        context ['url'] = fs.url(name) #mandando pro template
-    return render(request,'upload.html', context) #tem que chamar ele no front
+
+    try:
+        if request.method == 'POST':
+            uploaded_file = request.FILES['document']
+            fs = FileSystemStorage()
+            name = fs.save(uploaded_file.name, uploaded_file)
+            url = context['url'] = fs.url(name)
+
+            new_upload = UploadFile(document=url)
+            new_upload.save()
+
+    except Exception as e:
+        pass
+
+    return render(request, 'upload.html', context)
